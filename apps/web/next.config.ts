12325ae80e00510b18325a4b77380d461a418ee1
vamshi <vamshikrishna9246@gmail.com>
2026-04-16 20:29:31 +0530

    removed transpile

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: [
-    "@repo/api",
-    "@repo/core",
-    "@repo/hooks",
-    "@repo/redux",
-    "@repo/services",
-    "@repo/types",
-  ],
-
   experimental: {
     externalDir: true,
   },
