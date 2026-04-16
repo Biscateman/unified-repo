3674959afc878b1fb756810d6ba3538a7ce7c8c7
vamshi <vamshikrishna9246@gmail.com>
2026-04-16 14:55:49 +0530

    change

--- apps/web/src/lib/apiClient.ts
+++ apps/web/src/lib/apiClient.ts
@@ -2,8 +2,8 @@ import { createApiClient } from "@repo/api";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-if (!BASE_URL) {
-  throw new Error("NEXT_PUBLIC_BASE_URL is not defined in the environment.");
-}
+// if (!BASE_URL) {
+//   throw new Error("NEXT_PUBLIC_BASE_URL is not defined in the environment.");
+// }
 
-export const apiClient = createApiClient(BASE_URL);
+export const apiClient = createApiClient(BASE_URL!);
